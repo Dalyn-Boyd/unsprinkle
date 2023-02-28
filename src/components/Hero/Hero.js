7e78d1af7f7530e883282a2708c489e80f672661
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-02-28 15:54:49 -0600

    finished workshop:

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,29 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type='image/avif'
+          srcSet={`
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x
+          `}
+        />
+        <source
+          type='image/jpeg'
+          srcSet={`
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x
+          `}
+        />
+        <HeroImage
+          alt='An example of the photos distributed through this site (cat on black background)'
+          src='/images/hero-img.jpg'
+        />
+      </picture>
+      <Swoop alt='' src='/swoop.svg' />
     </Wrapper>
   );
 };
@@ -25,6 +46,7 @@ const HeroImage = styled.img`
   width: 500px;
   height: 500px;
   max-height: 100%;
+  object-fit: cover;
 `;
 
 const Swoop = styled.img`
